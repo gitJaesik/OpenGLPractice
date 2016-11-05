978cef0694e366d42925f822a29653385d3264ce
gitJaesik <maguire1815@gmail.com>
2016-11-05 23:42:17 +0900

    First project make triangle

---
+++ ConsoleOpenGL/ConsoleOpenGL/ConsoleOpenGL.cpp
@@ -0,0 +1,21 @@
+#include "glut.h"
+
+void DoDisplay()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_TRIANGLES);
+	glVertex2f(0.0, 0.5);
+	glVertex2f(-0.5, -0.5);
+	glVertex2f(0.5, -0.5);
+	glEnd();
+	glFlush();
+}
+
+void main()
+{
+	glutCreateWindow("OpenGL");
+	glutDisplayFunc(DoDisplay);
+	glutMainLoop();
+	
+}
